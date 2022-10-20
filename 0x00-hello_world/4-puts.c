45b9a9ae03721539b4c0db0e802f4296857be496
Samuel Bolarinwa <sukysamcy@gmail.com>
2022-10-20 16:53:07 -0700

    first code

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
+
 int main(void)
 
 {
-	char myStr[] = "Programming is like building a multilingual puzzle";
+	char myStr[] = "\"Programming is like building a multilingual puzzle";
 
 	puts(myStr);
 	return (0);
