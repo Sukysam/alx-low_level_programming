2dfccc7b911fc5e7efc80534df53ada9bc47e29d
Samuel Bolarinwa <sukysamcy@gmail.com>
2022-10-21 09:17:09 -0700

    print error

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -4,9 +4,8 @@
  *
  * Return: Always 1
  */
-
 int main(void)
 {
-	fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	fprintf("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
 	return (1);
 }
