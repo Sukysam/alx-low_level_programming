0c5b034cf3405c0ff6e894bc1efe6eb7517761f7
Samuel Bolarinwa <sukysamcy@gmail.com>
2022-10-21 07:08:53 -0700

    sizeof

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -2,7 +2,6 @@
 /**
  * main - Entry point
  *
- * sizeof() - print size of types
  *
  * Return: Always 0 (Success)
  */
@@ -12,12 +11,12 @@ int main(void)
 	float b;
 	char c;
 	long int d;
-	long long int e;
+	long long int j;
 
 	printf("Size of an int: %ld byte(s)\n", (unsigned long)sizeof(a));
 	printf("Size of a float: %ld byte(s)\n", (unsigned long)sizeof(b));
 	printf("Size of a char: %ld byte(s)\n", (unsigned long)sizeof(c));
 	printf("Size of a long int: %ld byte(s)\n", (unsigned long)sizeof(d));
-	printf("Size of a long long int: %ld byte(s)", (unsigned long)sizeof(e));
+	printf("Size of a long long int: %ld byte(s)", (unsigned long)sizeof(j));
 	return (0);
 }
