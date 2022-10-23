2a079237b3d9fe8682d5a906fe1ae05b6f71fc31
Samuel Bolarinwa <sukysamcy@gmail.com>
2022-10-23 01:00:42 -0700

    last digit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -15,8 +15,6 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 
-	while (n > 0)
-	{
 	n = n % 10;
 	if (n > 5)
 	{
@@ -29,7 +27,6 @@ int main(void)
 	else if (n < 6 && n != 0)
 	{
 		printf("Last digit on n is %d and is less than 6 and not 0\n", n);
-	}
 	}
 	return (0);
 }
