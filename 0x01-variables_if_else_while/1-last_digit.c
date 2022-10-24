f7b79d41de3ef2acedcaceaf86f9b3f382ce3ee2
Samuel Bolarinwa <sukysamcy@gmail.com>
2022-10-24 03:16:29 -0700

    last number

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -17,11 +17,13 @@ int main(void)
 	n = rand() - RAND_MAX / 2;
 
 	ld = n % 10;
+	n = ld;
 	if (n >= 5)
 	{
 		printf("Last digit of %d is %d and is greater than 5\n", n, ld);
 	}
-	else if (n <= 0)
+	
+	else if (n == 0)
 	{
 		printf("Last digit of %d is %d and is 0\n", n, ld);
 	}
