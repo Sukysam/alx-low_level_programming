2f855c9d04794a27b3353577bdecbabde740b81a
Samuel Bolarinwa <sukysamcy@gmail.com>
2022-10-24 08:01:13 -0700

    base 10

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int i = 0;
+
+	while (i >= 0 && i <= 9)
+	{
+	printf("%d \n", i);
+	i++;
+	}
+	return (0);
+}
