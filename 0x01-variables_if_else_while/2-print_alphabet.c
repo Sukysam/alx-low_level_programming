d3356197a0995927a494ed6f327022a23accd812
Samuel Bolarinwa <sukysamcy@gmail.com>
2022-10-24 04:11:39 -0700

    lowercase

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char l = 'a';
+
+	while (l >= 0)
+	{
+	putchar(l);
+	putchar('\n');
+	l++;
+	}
+	return (0);
+}
