60bc3dcdf0c6dd2839ab59e5bebdab953edebbd6
Samuel Bolarinwa <sukysamcy@gmail.com>
2022-10-24 05:14:57 -0700

    lowercaer and uppercase

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char l = 'a';
+	char u = 'A';
+
+	while (l <= 'z')
+	{
+	putchar(l);
+	l++;
+	}
+	while (u <= 'Z')
+	{
+	putchar(u);
+	u++;
+	}
+	putchar('\n');
+	return (0);
+}
