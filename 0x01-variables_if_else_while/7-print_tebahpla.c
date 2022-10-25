8e613175badc829ba9f13a018d3d7f3787698deb
Samuel Bolarinwa <sukysamcy@gmail.com>
2022-10-25 00:58:17 -0700

    base 10 and alphert revers

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char l = 'a';
+
+	while (l >= 'z')
+	{
+	putchar(l);
+	l--;
+	}
+	putchar('\n');
+	return (0);
+}
