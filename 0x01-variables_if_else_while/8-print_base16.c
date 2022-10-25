b3c64a453864ede8aa198e1a189abd5a4a43c11f
Samuel Bolarinwa <sukysamcy@gmail.com>
2022-10-25 04:25:26 -0700

    numberz and letters

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -10,11 +10,16 @@
 int main(void)
 {
 	int i = 0;
+	char l;
 
-	while (i >= 0 && i <= 15)
+	while (i >= 0 && i <= 9 )
 	{
 	putchar((i % 10) + '0');
 	i++;
+	}
+	for (l = 'a'; l <= 'f'; l++)
+	{
+	putchar(l);
 	}
 	putchar('\n');
 	return (0);
