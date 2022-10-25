8ac799d3815327bc94a80e6cde2da1f0df885375
Samuel Bolarinwa <sukysamcy@gmail.com>
2022-10-25 10:52:13 -0700

    print with coomas

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int d1, n = 0;
+	
+	for (d1 = '0'; d1 <= '9'; d1++)
+	{
+	putchar(d1);
+	putchar(',');
+	putchar(' ');
+	}
+	putchar('\n');
+	return (0);
+}
