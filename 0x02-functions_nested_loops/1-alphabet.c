1d85461a74a47c073f7b6c6b438b0256c725e5fe
Samuel Bolarinwa <sukysamcy@gmail.com>
2022-10-27 02:07:09 -0700

    print alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -6,10 +6,7 @@
  * Return: Always 0 (Success)
  */
 
-void print_alphabet(void);
-
-int main(void)
-
+void print_alphabet(void)
 {
 	int l;
 
@@ -18,5 +15,4 @@ int main(void)
 	_putchar((l % 10) + '0');
 	_putchar('\n');
 	}
-	return (0);
 }
