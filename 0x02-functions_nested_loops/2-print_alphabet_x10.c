75b3e800ff82d0537ea8c39028ab06e7c74667e1
Samuel Bolarinwa <sukysamcy@gmail.com>
2022-10-27 02:57:04 -0700

    print lowercase 10 times

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - Print all alphabet in lowercase 10 times
+ */
+
+void print_alphabet_x10(void)
+{
+	char l;
+	int j;
+
+	for (j = 0; j <= 9; j++)
+	{
+	for (l = 'a'; l <= 'z'; l++)
+	_putchar('\n');
+	}
+	_putchar((j % 10) + '0');
+}
