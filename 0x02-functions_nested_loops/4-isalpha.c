19d154370db3600817d3a2cca3b41467bd233bf8
Samuel Bolarinwa <sukysamcy@gmail.com>
2022-10-27 05:09:21 -0700

    checking char u/l

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _isalpha - check if a char is lowercase or uppercase
+ * @c: is the char to be checked
+ * Return: 1 if char is lowercase or uppercase, otherwise 0
+ */
+
+int _isalpha(int c)
+{
+	if (c >= 'A' && c <= 'Z' && c >= 'a' && c <= 'z')
+	{
+	return (1);
+	}
+	else
+	{
+	return (0);
+	}
+	_putchar('\n');
+}
