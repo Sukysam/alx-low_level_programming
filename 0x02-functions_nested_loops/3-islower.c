b5ebfbdcd6c3f008b6aff23b805286033bf26c10
Samuel Bolarinwa <sukysamcy@gmail.com>
2022-10-27 04:12:42 -0700

    print if c is lower or uppercase

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _islower - Print if 'c' is lowercase or uppercase
+ *
+ * Return: 0 or 1
+ */
+
+int _islower(int c)
+{
+	char ch = 'c';
+
+	if (ch == 'C')
+	{
+	_putchar('1');
+	}
+	else
+	{
+	_putchar('0');
+	}
+	_putchar('\n');
+}
