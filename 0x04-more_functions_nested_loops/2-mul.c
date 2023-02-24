43541bdda00c4c01995973f577c7427cd7897e09
Samuel Bolarinwa <sukysamcy@gmail.com>
2023-02-24 07:47:56 -0800

    2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * mul - A function to multiply tow veriables
+ * @a: First parameter
+ * @b: Second Parameter
+ * Return: (0)
+ */
+
+int mul(int a, int b)
+{
+	int multiply = a * b;
+
+	return (multiply);
+}
