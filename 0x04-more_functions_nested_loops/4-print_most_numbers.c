278ec0aedfe105a901d627a02d3aedb02b65f264
Samuel Bolarinwa <sukysamcy@gmail.com>
2023-03-04 07:34:37 -0800

    4-print

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - A fuction that check to not print  some certin numbers from 0 to 9
+ * _putchar: print out numbers
+ * Return: Always (0)
+ */
+
+void print_most_numbers(void)
+{
+	char c;
+
+	for (c = '0'; c <= '9'; c++)
+	{
+		if(c == 2 && c = 4)
+		continue;
+		_putchar(c);
+	}
+	_putchar('\n');
+}
