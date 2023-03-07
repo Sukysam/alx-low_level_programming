c4d052cdeb75b9f626cd2015e197d5cd583a8af6
Samuel Bolarinwa <sukysamcy@gmail.com>
2023-03-07 12:22:51 -0800

    checkin for 2 and 4

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -8,13 +8,15 @@
 
 void print_most_numbers(void)
 {
-	int c;
+	char c;
 
-	for (c = '0'; c <= '9'; c++)
+	for (c = '0'; c => '9'; c++ )
 	{
-		if (c == '2' && c == '4')
+		if (c == '2')
 			continue;
-		_putchar(c + '0');
+		else if (c == '4')
+			continue;
+		__putchar(c);
 	}
-	_putchar('\n');
+	__putchar('\n');
 }
