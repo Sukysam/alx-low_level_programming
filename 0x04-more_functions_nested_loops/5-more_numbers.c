6e45fdbafc0d304f8493d57cc21438d765767151
Samuel Bolarinwa <sukysamcy@gmail.com>
2023-03-08 01:07:41 -0800

    more numbers

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -8,14 +8,14 @@
 
 void more_numbers(void)
 {
-	char c;
-	char j;
+	int c;
+	int j;
 
-	for (c = '0'; c <= '9'; c++)
+	for (c = 0; c <= 9; c++)
 	{
-		for (j = '0'; j <= '14'; j++)
-			_putchar(j);
+		for (j = 0; j <= 14; j++)
+		_putchar(j + '0');
 		_putchar('\n');
 	}
-	_putchar(c);
+	_putchar(c + '0');
 }
