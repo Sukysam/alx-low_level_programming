afcc15e05334a7175636746cb0b02d6cb2b2ede4
Samuel Bolarinwa <sukysamcy@gmail.com>
2023-03-17 06:28:20 -0700

    7 //

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -13,18 +13,18 @@ void print_diagonal(int n)
 	{
 		int i, j;
 		{
-		for (i = 0; i < n; i++)
-			for (j = 0; j < n; j++)
-			{
-				if (j == i)
+			for (i = 0; i < n; i++)
+				for (j = 0; j < n; j++)
 				{
-					_putchar('\\');
-					_putchar('\n');
+					if (j == i)
+					{
+						_putchar('\\');
+						_putchar('\n');
+					}
+					else if (j < i)
+						_putchar(' ');
 				}
-				else if (j < i)
-					_putchar(' ');
-			}
-		_putchar('\n');
+			_putchar('\n');
 		}
 	}
 }
